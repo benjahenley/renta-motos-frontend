4db000a0644fcf4298379273fcede81d38b3f10a
Facundo <facundopohle@gmail.com>
2024-06-05 11:41:08 -0300

    payment

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -3,9 +3,7 @@
 import React from 'react';
 import PaymentForm from '@/components/payment/paymentform';
 import ListingDetailsHeader from '@/components/header/listing-details';
-import { TabItem, Tablist, TabPanel, TabPanels } from '@/components/ui/tab';
-import { Tab } from '@headlessui/react';
-import Carrito from '@/components/payment/carrito'
+import Carrito from '@/components/payment/carrito';
 
 export default function PaymentPage({
   children,
@@ -13,13 +11,16 @@ export default function PaymentPage({
   return (
     <>
       <ListingDetailsHeader />
-      <div className="container-fluid mt-5 grid !max-w-[1280px] grid-cols-1 gap-5 pb-10 md:mt-7 lg:grid-cols-3 xl:mt-12 xl:grid-cols-3 xl:gap-8 2xl:gap-12 3xl:mt-16 3xl:!px-0">
-        <div className="lg:col-span-1">
-          <Carrito />
-        </div>
-        <div className="lg:col-span-2">
-          <div className="container mx-auto p-4">
-            <PaymentForm />
+      <div className="container-fluid mt-5 pb-10 md:mt-7 xl:mt-12 3xl:mt-16 3xl:!px-0">
+        <div className="flex flex-col lg:flex-row justify-start gap-5 lg:gap-8 2xl:gap-12">
+          <div className="lg:w-1/3">
+            <Carrito />
+          </div>
+          <div className="lg:w-2/3">
+            <main className="flex-grow">{children}</main>
+            <div className="p-4">
+              <PaymentForm />
+            </div>
           </div>
         </div>
       </div>
