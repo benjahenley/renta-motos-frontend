3586954320775ec020cc1d942812e87a81a6372a
Facundo <facundopohle@gmail.com>
2024-06-16 23:11:28 -0300

    delete

---
+++ src/api/reservations/deleteReservation.ts
@@ -0,0 +1,23 @@
+import { getToken } from '@/helpers/getToken';
+
+export const deleteReservation = async (id: string) => {
+  const url = `${process.env.NEXT_PUBLIC_URL_API_SERVER}/reservations/${id}`;
+  try {
+    const token = getToken();
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.error || 'An unknown error occurred');
+    }
+
+    return await response.json();
+  } catch (error: any) {
+    throw new Error(error.message || 'An unknown error occurred');
+  }
+};
