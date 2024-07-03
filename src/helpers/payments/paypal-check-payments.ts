2907a687f97e7bc743993857c33b837aea298967
DevWebPM <webs@panoramaminero.com.ar>
2024-07-03 08:14:08 -0300

    Updated PayPal integration and environment variables

--- src/helpers/payments/paypal-check-payments.ts
+++ src/helpers/payments/paypal-check-payments.ts
@@ -25,7 +25,7 @@ export const paypalCheckPayment = async (paypalTransactionId: string) => {
 };
 
 const getPaypalBearerToken = async (): Promise<string | null> => {
-  const PAYPAL_CLIENT_ID = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID;
+  const PAYPAL_CLIENT_ID = process.env.PAYPAL_CLIENT_ID;
   const PAYPAL_SECRET = process.env.PAYPAL_SECRET;
   const oauth2Url = process.env.NEXT_PUBLIC_PAYPAL_OAUTH_URL ?? '';
 
